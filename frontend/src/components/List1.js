9fdaaa40b27bb0cce6b9d2723f4c15bca27729e8
Seung <wwsa123@naver.com>
2022-02-07 18:08:57 +0900

    2.7.1
    api 오류 해결

--- frontend/src/components/List1.js
+++ frontend/src/components/List1.js
@@ -11,23 +11,31 @@ function List1(){
 //     })
 // },[] )
 
+const [counseling, setCounseling] = useState(null);
 const axios = require("axios")
-const url = encodeURI("/apis/esd/Gym_counseling/records?성별=남성");
-const headers = {
-    "coginsight-api-key" : `roebQelczjsdRVqwtKG6ezTZInQfBeUnrREDoy7dTrGbvwkYxJzgmVMMw6q7p83fjsCZ3cuIVbuwFcNoyhpjEQ==|pt0f0hklgSYGCL0dhmtpzF5ngvh3QPcVNDUhqGXJ1D0=` ,
-    "coginsight-domain-id" : `4f33c78d-678b-4f8d-a62c-54f54061139c`
-}
 
-axios.get(url,{headers}).then((response) =>{
-    console.log(response)
-})
-
-
+useEffect( () => {
+          const url = encodeURI("/apis/esd/Gym_counseling/records");
+          const headers = {
+           "coginsight-api-key" : `roebQelczjsdRVqwtKG6ezTZInQfBeUnrREDoy7dTrGbvwkYxJzgmVMMw6q7p83fjsCZ3cuIVbuwFcNoyhpjEQ==|pt0f0hklgSYGCL0dhmtpzF5ngvh3QPcVNDUhqGXJ1D0=` ,
+           "coginsight-domain-id" : `4f33c78d-678b-4f8d-a62c-54f54061139c`
+          } 
+          axios.get(url,{headers}).then((response) =>{
+            setCounseling(response.data.result)
+            
+          })
+}, [])
+console.log(counseling)
     return(
         <div className ="list">
             <h3>리스트</h3>
             <hr></hr>
             <ul>
+            {counseling.map(user => (
+                    <li key={user.id}>
+                        {user.name} ({user.age})
+                    </li>
+                ))}
             </ul>
             
         </div>
