268986b851be56e749a1a61ad7d5220943258565
Seung <wwsa123@naver.com>
2022-02-16 17:34:42 +0900

    2.16.2

--- frontend/src/pages/Explain.js
+++ frontend/src/pages/Explain.js
@@ -18,36 +18,70 @@ function Explain(){
     }
 
     
-    useEffect( ()=>{
-        setExercise(
-            [
-                {
-                    name : "사이드레터럴레이즈",
-                    exercise : "어깨운동"
-                },
-                {
-                    name : "벤치프레스",
-                    exercise : "가슴운동"
-                },
-                {
-                    name : "풀업",
-                    exercise : "등운동"
-                },
-                {
-                    name : "사이드레터럴레이즈",
-                    exercise : "어깨운동"
-                },
-                {
-                    name : "벤치프레스",
-                    exercise : "가슴운동"
-                },
-                {
-                    name : "풀업",
-                    exercise : "등운동"
-                }
+    // class App extends React.Component{
+    //     constructor(props){
+    //         super(props);
+    //         this.state=
+    //         {
+    //             users:[],
+    //             sub:'',
+    //             detail:'',
+    //             id:0
+    //         }
+    //     }
+    //     componentDidMount(){
+    //         axios.get('http://localhost:5000/')
+    //         .get((res)=>
+    //         this.setState({
+    //             users:res.data,
+    //             sub:'',
+    //             detail:'',
+    //             id:0
+    //         })
+    //         )
+    //     }
 
-            ]
-        )
+    
+    // useEffect( ()=>{
+    //     setExercise(
+    //         [
+    //             {
+    //                 name : "사이드레터럴레이즈",
+    //                 exercise : "어깨운동"
+    //             },
+    //             {
+    //                 name : "벤치프레스",
+    //                 exercise : "가슴운동"
+    //             },
+    //             {
+    //                 name : "풀업",
+    //                 exercise : "등운동"
+    //             },
+    //             {
+    //                 name : "사이드레터럴레이즈",
+    //                 exercise : "어깨운동"
+    //             },
+    //             {
+    //                 name : "벤치프레스",
+    //                 exercise : "가슴운동"
+    //             },
+    //             {
+    //                 name : "풀업",
+    //                 exercise : "등운동"
+    //             }
+
+    //         ]
+    //     )
+    // }, [])
+
+
+    const axios = require("axios")
+    useEffect( () => {
+            const url = encodeURI("http://localhost:5000/");
+            axios.get(url).then((response) =>{
+                setExercise(response.data.result)
+                
+            })
     }, [])
     console.log(exercise)
     if (!exercise) return null;
